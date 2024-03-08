fa2da3c5b282208fec2eeef0d8416daeccf02681
olynad <danilnagornyi2015@gmail.com>
2024-03-08 07:42:15 -0700

    lab 8

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -53,8 +53,12 @@ public class CustomList extends ArrayAdapter<City> {
         cities.add(city);
     }
 
-    public boolean hasCity(String city) {
-
+    public boolean hasCity(String cityName) {
+        for (City city : cities) {
+            if (city.getCityName().equals(cityName)) {
+                return true;
+            }
+        }
         return false;
     }
 
